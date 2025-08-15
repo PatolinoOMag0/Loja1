2bf7337263fa56700e95bcde9d280fe9f8bb4d00
PatolinoOMag0 <161628377+PatolinoOMag0@users.noreply.github.com>
2025-08-15 17:28:35 -0300

    Meme

---
+++ k/listar.php
@@ -0,0 +1,156 @@
+<?php
+    include 'cabecalho.php';
+?>
+<body>
+    <div class="container">
+        <h1>Bem vindo ao 1º Sistema com CRUD</h1>
+        <h2>Kevin Flamenguista</h2>
+    </div>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">NOME</th>
+                    <th scope="col">Vulgo</th>
+                    <th scope="col">PREÇO</th>
+                    <th scope="col">QUANTIDADE</th>
+                    <th scope="col">OPÇÕES  </th>
+                </tr>
+                
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Kevin</td>
+                    <td>Flamenguista</td>
+                    <td>Inestimavel</td>
+                    <td>1</td>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <a href="#" type="button" class="btn btn-success">Comprar</a>
+                            <a href="#" type="button" class="btn btn-outline-danger" for="vbtn-radio2">Exclói</a>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Matheus</td>
+                    <td>Tzão da etexc</td>
+                    <td>Não vale nada</td>
+                    <td>1</td>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <a href="#" type="button" class="btn btn-success">Comprar</a>
+                            <a href="#" type="button" class="btn btn-outline-danger" for="vbtn-radio2">Exclói</a>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Arthur</td>
+                    <td>Chupingole</td>
+                    <td>De Graça</td>
+                    <td>1</td>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <a href="#" type="button" class="btn btn-success">Comprar</a>
+                            <a href="#" type="button" class="btn btn-outline-danger" for="vbtn-radio2">Exclói</a>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">4</th>
+                    <td>Ryan</td>
+                    <td>Neguinho</td>
+                    <td>1545</td>
+                    <td>1</td>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <a href="#" type="button" class="btn btn-success">Comprar</a>
+                            <a href="#" type="button" class="btn btn-outline-danger" for="vbtn-radio2">Exclói</a>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">5</th>
+                    <td>Julia</td>
+                    <td>Piracanjulia</td>
+                    <td>Twink</td>
+                    <td>1</td>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <a href="#" type="button" class="btn btn-success">Comprar</a>
+                            <a href="#" type="button" class="btn btn-outline-danger" for="vbtn-radio2">Exclói</a>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">6</th>
+                    <td>Julia</td>
+                    <td>Vitão</td>
+                    <td>Mole</td>
+                    <td>1</td>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <a href="#" type="button" class="btn btn-success">Comprar</a>
+                            <a href="#" type="button" class="btn btn-outline-danger" for="vbtn-radio2">Exclói</a>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">7</th>
+                    <td>Mirela</td>
+                    <td>Batman</td>
+                    <td>Justiça</td>
+                    <td>1</td>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <a href="#" type="button" class="btn btn-success">Comprar</a>
+                            <a href="#" type="button" class="btn btn-outline-danger" for="vbtn-radio2">Exclói</a>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">8</th>
+                    <td>Ana</td>
+                    <td>Cracuda</td>
+                    <td>Pó de 10</td>
+                    <td>1</td>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <a href="#" type="button" class="btn btn-success">Comprar</a>
+                            <a href="#" type="button" class="btn btn-outline-danger" for="vbtn-radio2">Exclói</a>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">9</th>
+                    <td>Maria</td>
+                    <td>Opala</td>
+                    <td>Jack Daniel's</td>
+                    <td>1</td>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <a href="#" type="button" class="btn btn-success">Comprar</a>
+                            <a href="#" type="button" class="btn btn-outline-danger" for="vbtn-radio2">Exclói</a>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">10</th>
+                    <td>Kaliane</td>
+                    <td>KaliKali</td>
+                    <td>Ex</td>
+                    <td>1</td>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <a href="#" type="button" class="btn btn-success">Comprar</a>
+                            <a href="#" type="button" class="btn btn-outline-danger" for="vbtn-radio2">Exclói</a>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
