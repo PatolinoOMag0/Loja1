ffc21b064543787bed0834295d8a4a742ad7a013
PatolinoOMag0 <161628377+PatolinoOMag0@users.noreply.github.com>
2025-08-22 18:23:46 -0300

    arumei os bglh tudo

---
+++ form_cadastrar.php
@@ -0,0 +1,21 @@
+<?php
+    include 'cabecalho.php';
+?>
+<body>
+    <div class="container">
+        <h2>CADASTRO DE PRODUTO</h2>
+    <form action= "inserir.php" method="POST">
+        <div class="mb-3">
+            <input type="text" name="produto" class="form-control" placeholder="Digite o nome do Produto">
+        </div>
+        <div class="mb-3">
+            <input type="text" name="preco" class="form-control" placeholder="Digite o preço do Produto">
+        </div>
+        <div class="mb-3">
+            <input type="text" name="quantidade" class="form-control" placeholder="Digite a quantidade do Produto">
+        </div>
+        <button type="submit" class=btn btn-primary>Cadastrar</button>
+    </form>
+    
+</body>
+</html>
---
+++ listar.php
@@ -0,0 +1,46 @@
+<?php
+    include 'cabecalho.php';
+?>
+<body>
+    <div class="container">
+        <h1>Bem vindo ao 1º Sistema com CRUD</h1>
+        <h2>Kevin Flamenguista</h2>
+    </div>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">NOME</th>
+                    <th scope="col">Vulgo</th>
+                    <th scope="col">PREÇO</th>
+                    <th scope="col">QUANTIDADE</th>
+                    <th scope="col">OPÇÕES  </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    require 'conexao.php';
+                    $sql = "SELECT * FROM produtos";
+                    $stmt = $pdo->query($sql);
+                    while ($produto = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        echo "<tr>";
+                        echo "<td>".$produto['id']."</td>";
+                        echo "<td>".$produto['nome']."</td>";
+                        echo "<td>".$produto['preco']."</td>";
+                        echo "<td>".$produto['quantidade']."</td>";
+                        echo "
+                        <td>
+                            <div class='btn-group' role='group' aria-label='Basic mixed styles example'>
+                                <a href='#' type='button' class='btn btn-success'>Comprar</a>
+                                <a href='#' type='button' class='btn btn-outline-danger' for='vbtn-radio2'>Apagar</a>
+                            </div>
+                        </td>
+                        ";
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
